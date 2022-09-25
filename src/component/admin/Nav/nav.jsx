4055ad5b0a74d56af27094e17c03d8c318d5afaf
helmy16 <medohmada1623@gmail.com>
2022-09-25 20:12:07 +0200

    responsive nav bar

--- src/component/admin/Nav/nav.jsx
+++ src/component/admin/Nav/nav.jsx
@@ -4,33 +4,74 @@ import './nav.css';
 import Popup from "../../popup/popup";
 import tick from "../../props/tick.svg";
 import Services from "../../services/services";
+import styled from 'styled-components';
+import ResponsiveMenu from 'react-responsive-navbar';
+import { FaBars, FaWindowClose } from 'react-icons/fa';
+import { MdOutlineClose} from 'react-icons/md';
+
+const Menu = styled.div`
+  border-bottom: 2px solid #72BFD0;
+  ul {
+    padding: 0;
+  }
+  li {
+    display: inline;
+    font-size: 13px;
+    list-style-type: none;
+    margin-left: 30px;
+  }
+  a {
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 20px;
+    color: MediumPurple;
+    &:hover {
+      color: white;
+    }
+  }
+  @media (max-width: 500px) {
+    padding: 10px 0;
+    li {
+      padding: 10px 0;
+      display: block;
+      margin-left: 0;
+    }
+  }
+`;
 
 const Nav = (props) => { 
     const [IsPopupOpen, setIsPopupOpen] = useState(false);
     return (
-            <div className="nav">
-                <div className="leftnav">
-                    <Link to="/dashboard" style={{textDecoration:'none'}}><div className={props.active==="Dashboard"?"nav-item active":"nav-item"}>Dashboard</div></Link>
-                    <Link to="/report" style={{textDecoration:'none'}}><div className={props.active==="Report"?"nav-item active":"nav-item"}>Report</div></Link>
-                    <Link to="/roomsetup" style={{textDecoration:'none'}}><div className={props.active==="Room"?"nav-item active":"nav-item"}>Room Setup</div></Link>
-                </div>
-                <div className="rightnav">
-                    <button className="nav-item add-service" onClick={() => setIsPopupOpen(true)}>Add Service</button>
-                    <button className="nav-item" style={{background: 'transparent', border: 'none'}}>Logout</button>
-                </div>
-            
-              {IsPopupOpen && (
-                <Popup
-                  noclose= {false}
-                  content={
-                    <>
-                      <Services nonav={true}></Services>
-                    </>
-                  }
-                  handleClose={e => setIsPopupOpen(!IsPopupOpen)}
-                />
-              )}
-            </div>
+      <div style={{width:'100%'}}>
+        <ResponsiveMenu
+          menuOpenButton={<FaBars size={30} color="#72BFD0" />}
+          menuCloseButton={<MdOutlineClose size={30} color="#72BFD0" />}
+          changeMenuOn="850px"
+          largeMenuClassName="nav"
+          smallMenuClassName="nav2"
+          menu={
+            <Menu>
+              <ul>
+                <li>
+                  <Link to="/dashboard">Dashboard Page</Link>
+                </li>
+                <li>
+                  <Link to="/report">Report Page</Link>
+                </li>
+                <li>
+                <Link to="/roomsetup">Room Setup Page</Link>
+                </li>
+                <li>
+                  <button> Add Service</button>
+                </li>
+                <li>
+                  <a href="">Logout</a>
+                </li>
+              </ul>
+            </Menu>
+          }
+        />
+      </div>
     );
   };
   export default Nav;
